82107bf3371ed4b66ca2bb21b97b0cbf826717aa
Pooja <pthakare33@gmail.com>
2016-07-15 11:24:10 +0530

    only sidemenu :tired_face:

---
+++ api/controllers/DharmatvController.js
@@ -0,0 +1,68 @@
+module.exports = {
+
+  save: function(req, res) {
+    if (req.body) {
+      Dharmatv.saveData(req.body, res.callback);
+    } else {
+      res.json({
+        value: false,
+        data: "Invalid Request"
+      });
+    }
+  },
+
+  getOne: function(req, res) {
+
+    if (req.body) {
+      Dharmatv.getOne(req.body, res.callback);
+    } else {
+      res.json({
+        value: false,
+        data: "Invalid Request"
+      });
+    }
+  },
+
+  delete: function(req, res) {
+    if (req.body) {
+      Dharmatv.deleteData(req.body, res.callback);
+    } else {
+      res.json({
+        value: false,
+        data: "Invalid Request"
+      });
+    }
+  },
+
+  getAll: function(req, res) {
+    function callback(err, data) {
+      Global.response(err, data, res);
+    }
+    if (req.body) {
+      Dharmatv.getAll(req.body, res.callback);
+    } else {
+      res.json({
+        value: false,
+        data: "Invalid Request"
+      });
+    }
+  },
+
+  findLimited: function(req, res) {
+    if (req.body) {
+      if (req.body.pagenumber && req.body.pagenumber !== "" && req.body.pagesize && req.body.pagesize !== "") {
+        Dharmatv.findLimited(req.body, res.callback);
+      } else {
+        res.json({
+          value: false,
+          data: "Please provide parameters"
+        });
+      }
+    } else {
+      res.json({
+        value: false,
+        data: "Invalid Request"
+      });
+    }
+  },
+};
