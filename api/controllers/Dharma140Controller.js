acca7d23fa59611df7a589be1e3305e28936851a
Chintan <chintan@wohlig.com>
2016-08-17 12:33:27 +0530

    changes  for the same

--- api/controllers/Dharma140Controller.js
+++ api/controllers/Dharma140Controller.js
@@ -5,77 +5,83 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 module.exports = {
-  save: function(req, res) {
-    if (req.body) {
-      Dharma140.saveData(req.body, res.callback);
-    } else {
-      res.json({
-        value: false,
-        data: "Invalid Request"
-      });
-    }
-  },
+    save: function(req, res) {
+        if (req.body) {
+            Dharma140.saveData(req.body, res.callback);
+        } else {
+            res.json({
+                value: false,
+                data: "Invalid Request"
+            });
+        }
+    },
 
-  getOne: function(req, res) {
+    getOne: function(req, res) {
 
-    if (req.body) {
-      Dharma140.getOne(req.body, res.callback);
-    } else {
-      res.json({
-        value: false,
-        data: "Invalid Request"
-      });
-    }
-  },
+        if (req.body) {
+            Dharma140.getOne(req.body, res.callback);
+        } else {
+            res.json({
+                value: false,
+                data: "Invalid Request"
+            });
+        }
+    },
 
-  delete: function(req, res) {
-    if (req.body) {
-      if (req.body._id) {
-        Dharma140.deleteData(req.body, res.callback);
+    delete: function(req, res) {
+        if (req.body) {
+            if (req.body._id) {
+                Dharma140.deleteData(req.body, res.callback);
 
-      } else {
-        res.json({
-          value: false,
-          data: "Please provide id"
-        });
-      }
-    } else {
-      res.json({
-        value: false,
-        data: "Invalid Request"
-      });
-    }
-  },
+            } else {
+                res.json({
+                    value: false,
+                    data: "Please provide id"
+                });
+            }
+        } else {
+            res.json({
+                value: false,
+                data: "Invalid Request"
+            });
+        }
+    },
 
-  getAll: function(req, res) {
-    function callback(err, data) {
-      Global.response(err, data, res);
-    }
-    if (req.body) {
-      Dharma140.getAll(req.body, res.callback);
-    } else {
-      res.json({
-        value: false,
-        data: "Invalid Request"
-      });
-    }
-  },
+    getAll: function(req, res) {
+        function callback(err, data) {
+            Global.response(err, data, res);
+        }
+        if (req.body) {
+            Dharma140.getAll(req.body, res.callback);
+        } else {
+            res.json({
+                value: false,
+                data: "Invalid Request"
+            });
+        }
+    },
+    getHash: function(req, res) {
 
-  findLimited: function(req, res) {
-    if (req.body) {
-      if (req.body.pagenumber && req.body.pagenumber !== "" && req.body.pagesize && req.body.pagesize !== "") {
-        Dharma140.findLimited(req.body, res.callback);
-      } else {
-        res.json({
-          value: false,
-          data: "Please provide parameters"
-        });
-      }
-    } else {
-      res.json({
-        value: false,
-        data: "Invalid Request"
-      });
+
+        Config.getTweets("#pokemon", ["gaming_otb","YumiScott"], res.callback);
+
+    },
+
+    findLimited: function(req, res) {
+        if (req.body) {
+            if (req.body.pagenumber && req.body.pagenumber !== "" && req.body.pagesize && req.body.pagesize !== "") {
+                Dharma140.findLimited(req.body, res.callback);
+            } else {
+                res.json({
+                    value: false,
+                    data: "Please provide parameters"
+                });
+            }
+        } else {
+            res.json({
+                value: false,
+                data: "Invalid Request"
+            });
+        }
     }
-  }
 };
