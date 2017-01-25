36aea8a6dcb3d8095d218a7af207c607d82f3eea
Chaitalee <chaitalee.wohlig@gmail.com>
2017-01-25 12:25:29 +0530

    changes

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -186,7 +186,7 @@ firstapp.filter('uploadpath2', function () {
 
         if (input) {
             if (input.indexOf('https://') == -1) {
-                return "http://dharma-production.com/api/" + "download/" + input;
+                return "http://dharmaimages.tingdom.in/api/" + "download/" + input;
 
             } else {
                 return input;
