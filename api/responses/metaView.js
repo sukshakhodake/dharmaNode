a0bbf70e705cd83232565a8bcc5fe8597ac480c8
Chaitalee <chaitalee.wohlig@gmail.com>
2017-02-08 15:35:01 +0530

    changes

--- api/responses/metaView.js
+++ api/responses/metaView.js
@@ -1,24 +1,39 @@
-module.exports = function (data, options) {
+module.exports = function(data, options) {
     var req = this.req;
     var res = this.res;
     var sails = req._sails;
     if (!data) {
         data = {};
     }
-    console.log('reqqqqqqqqqqqqqqqqqqqqqqq',req);
-    console.log('dattttttttttttttttttta',data);
-    console.log('reqqqqqqqqqqqqqqqqqqqqqqqqqppppppppppppppppppppp',req.path);
-    
+    console.log('reqqqqqqqqqqqqqqqqqqqqqqq', req);
+    console.log('dattttttttttttttttttta', data);
+    console.log('reqqqqqqqqqqqqqqqqqqqqqqqqqppppppppppppppppppppp', req.path);
     var env = require("../../config/env/" + sails.config.environment + ".js");
-    var obj = {
-        jsFiles: jsFiles,
-        title: data.title,
-        description: data.description,
-        keywords: data.keywords,
-        adminurl: env.realHost + "/api/",
-        image: env.realHost + "/api/download/" + data.image,
-        url: env.realHost + req.path,
-    };
+    var scoerPath = req.path;
+    if(scoerPath.indexOf('/fan-corner-score') == -1){
+      console.log('iffffff score');
+      var obj = {
+          jsFiles: jsFiles,
+          title: data.title,
+          description: data.description,
+          keywords: data.keywords,
+          adminurl: env.realHost + "/api/",
+          image: env.realHost + "/api/download/" + data.image,
+          url: env.realHost + req.path,
+      };
+    }else{
+      console.log('elseeeeeee score');
+      var obj = {
+          jsFiles: jsFiles,
+          title: data.title,
+          description: data.description,
+          keywords: data.keywords,
+          adminurl: env.realHost + "/api/",
+          image: env.realHost + "/api/download/" + data.image,
+          url: env.realHost + '/fan-corner',
+      };
+    }
+
     if (!data.image) {
         delete obj.image;
     }
