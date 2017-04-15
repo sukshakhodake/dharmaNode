40693b935dedb25f2bcc86df848ff7b88e7b1f06
Chintan <chintan@wohlig.com>
2017-04-15 12:30:24 +0530

    ok

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -4,139 +4,141 @@ global["fs"] = require('fs');
 global["exec"] = require('child_process').exec;
 module.exports = {
 
-  save: function(req, res) {
-    if (req.body) {
-      User.saveData(req.body, res.callback);
-    } else {
-      res.json({
-        value: false,
-        data: "Invalid Request"
-      });
-    }
-  },
-
-  getOne: function(req, res) {
-
-    if (req.body) {
-      User.getOne(req.body, res.callback);
-    } else {
-      res.json({
-        value: false,
-        data: "Invalid Request"
-      });
-    }
-  },
-
-  delete: function(req, res) {
-    if (req.body) {
-      User.deleteData(req.body, res.callback);
-    } else {
-      res.json({
-        value: false,
-        data: "Invalid Request"
-      });
-    }
-  },
-
-  getAll: function(req, res) {
-    function callback(err, data) {
-      Global.response(err, data, res);
-    }
-    if (req.body) {
-      User.getAll(req.body, res.callback);
-    } else {
-      res.json({
-        value: false,
-        data: "Invalid Request"
-      });
-    }
-  },
-  loginFacebook: function(req, res) {
-    var callback = function(err, data) {
-      if (err || _.isEmpty(data)) {
-        res.json({
-          error: err,
-          value: false
-        });
-      } else {
-        if (data._id) {
-          req.session.user = data;
-          req.session.save(function(err) {
-            if (err) {
-              res.json(err);
-            } else {
-              res.redirect(redirect);
-            }
-          });
+    save: function (req, res) {
+        if (req.body) {
+            User.saveData(req.body, res.callback);
         } else {
-          res.json({
-            data: "User not found",
-            value: false
-          });
+            res.json({
+                value: false,
+                data: "Invalid Request"
+            });
         }
-      }
-    };
-    passport.authenticate('facebook', {
-      scope: ['public_profile', 'user_friends', 'email']
-    }, callback)(req, res);
-  },
+    },
 
-  profile: function(req, res) {
-    var user = req.session.user;
-    if (user) {
-      res.json({
-        data: user,
-        value: true
-      });
-    } else {
-      res.json({
-        data: "User not logged in",
-        value: false
-      });
-    }
-  },
-  loginTwitter: function(req, res) {
-    var callback = function(err, data) {
-      if (err || _.isEmpty(data)) {
-        res.json({
-          error: err,
-          value: false
-        });
-      } else {
-        req.session.user = data;
-        // console.log(req.session);
-        req.session.save(function(err) {
-          if (err) {
-            res.json(err);
-          } else {
-            res.json(
-              res.redirect(redirect)
-            );
-          }
-        });
-      }
-    };
-    passport.authenticate('twitter', {}, callback)(req, res);
-  },
+    getOne: function (req, res) {
 
-  findLimited: function(req, res) {
-    if (req.body) {
-      if (req.body.pagenumber && req.body.pagenumber !== "" && req.body.pagesize && req.body.pagesize !== "") {
-        User.findLimited(req.body, res.callback);
-      } else {
-        res.json({
-          value: false,
-          data: "Please provide parameters"
-        });
-      }
-    } else {
-      res.json({
-        value: false,
-        data: "Invalid Request"
-      });
-    }
-  },
+        if (req.body) {
+            User.getOne(req.body, res.callback);
+        } else {
+            res.json({
+                value: false,
+                data: "Invalid Request"
+            });
+        }
+    },
+
+    delete: function (req, res) {
+        if (req.body) {
+            User.deleteData(req.body, res.callback);
+        } else {
+            res.json({
+                value: false,
+                data: "Invalid Request"
+            });
+        }
+    },
+
+    getAll: function (req, res) {
+        function callback(err, data) {
+            Global.response(err, data, res);
+        }
+        if (req.body) {
+            User.getAll(req.body, res.callback);
+        } else {
+            res.json({
+                value: false,
+                data: "Invalid Request"
+            });
+        }
+    },
+    loginFacebook: function (req, res) {
+        var callback = function (err, data) {
+            if (err || _.isEmpty(data)) {
+                res.json({
+                    error: err,
+                    value: false
+                });
+            } else {
+                if (data._id) {
+                    req.session.user = data;
+                    req.session.save(function (err) {
+                        if (err) {
+                            res.json(err);
+                        } else {
+                            res.redirect(redirect);
+                        }
+                    });
+                } else {
+                    res.json({
+                        data: "User not found",
+                        value: false
+                    });
+                }
+            }
+        };
+        passport.authenticate('facebook', {
+            scope: ['public_profile', 'user_friends', 'email']
+        }, callback)(req, res);
+    },
+
+    profile: function (req, res) {
+        var user = req.session.user;
+        if (user) {
+            res.json({
+                data: user,
+                value: true
+            });
+        } else {
+            res.json({
+                data: "User not logged in",
+                value: false
+            });
+        }
+    },
+    loginTwitter: function (req, res) {
+        var callback = function (err, data) {
+            if (err || _.isEmpty(data)) {
+                res.json({
+                    error: err,
+                    value: false
+                });
+            } else {
+                req.session.user = data;
+                // console.log(req.session);
+                req.session.save(function (err) {
+                    if (err) {
+                        res.json(err);
+                    } else {
+                        res.json(
+                            res.redirect(redirect)
+                        );
+                    }
+                });
+            }
+        };
+        passport.authenticate('twitter', {}, callback)(req, res);
+    },
+
+    findLimited: function (req, res) {
+        if (req.body) {
+            if (req.body.pagenumber && req.body.pagenumber !== "" && req.body.pagesize && req.body.pagesize !== "") {
+                User.findLimited(req.body, res.callback);
+            } else {
+                res.json({
+                    value: false,
+                    data: "Please provide parameters"
+                });
+            }
+        } else {
+            res.json({
+                value: false,
+                data: "Invalid Request"
+            });
+        }
+    },
     backupDatabase: function (req, res) {
+        res.connection.setTimeout(200000000);
+        req.connection.setTimeout(200000000);
         var q = req.host.search("127.0.0.1");
         var database = "dharma";
         if (true) {
