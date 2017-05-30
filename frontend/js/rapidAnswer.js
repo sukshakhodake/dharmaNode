3d8295ad2843ba004b2839cda465767ef90948a8
Sohan Honakeri <shn619@gmail.com>
2017-05-30 17:50:52 +0530

    images remaining

--- frontend/js/rapidAnswer.js
+++ frontend/js/rapidAnswer.js
@@ -7,207 +7,207 @@ var rapidAnswer = angular.module('rapidAnswer', [])
         var questions = [{
             id: 1,
             quesNo: "Q1",
-            question:"How many films has Karan Johar directed for Dharma productions?",
+            question:"What is the name of Kalki Koechlin's character in the movie?",
             // question: "What was the surprise gift given by Rahul & Arjun to Dadu on his birthday?",
             model: "quest1",
             options: [{
                 "id": "radio1",
-                "name":"5"
+                "name":"Naina"
                 // "name": "Cut out of Manadakini",
             }, {
                 "id": "radio2",
-                "name":"6",
+                "name":"Aditi",
                   answer: true
                 // "name": "Cut out of Zeenat Aman"
 
             }, {
                 "id": "radio3",
                 // "name": "Cut out of Sridevi"
-                 "name":"7"
+                 "name":"Anjali"
             }, {
                 "id": "radio4",
-                "name":"9"
+                "name":"Neha"
                 // "name": "Cut out of Nargis"
             }]
         }, {
             id: 2,
             quesNo: "Q2",
-            question:"Which among the following songs is from a Karan Johar film?",
+            question:"Which song do Bunny and Avi sing to wake up Aditi?",
             // question: "What was the Kapoor family's pet dog's name?",
             model: "quest2",
             options: [{
                 "id": "radio1",
-                "name": "Bholi Si Soorat"
+                "name": " Badtameez Dil"
             }, {
                 "id": "radio2",
-                "name": "Bole Chudiyan",
-                 answer: true
+                "name": "Dilli wali Girlfriend"
             }, {
                 "id": "radio3",
-                "name": "Ik Pal Ka Jeena"  
+                "name": "Jumma Chumma De De",
+                 answer: true 
             }, {
                 "id": "radio4",
-                "name": "Tu Jaane Na"
+                "name": "Saara Zamaana"
             }]
         }, {
             id: 3,
             quesNo: "Q3",
-            question:"Which Karan Johar film has won a national award?",
+            question:"What is the name of the haunted hill that Bunny and Naina climb together?",
             // question: "What was Rahul’s profession in the movie?",
             model: "quest3",
             options: [{
                 "id": "radio1",
-                "name": "Kabhi Alvida Naa Kehna"
+                "name": "Bhutha Parbat", 
+                answer: true
             }, {
                 "id": "radio2",
-                "name": "My Name Is Khan"
+                "name": "Bhairav Parbat"
             }, {
                 "id": "radio3",
-                "name": "Kuch Kuch Hota Hai", 
-                answer: true 
+                "name": "Kaalmani Pahad"
             }, {
                 "id": "radio4",
-                "name": "Kabhi Khushi Kabhie Gham",
+                "name": "Raghav Pahad",
             }]
         }, {
             id: 4,
             quesNo: "Q4",
-            question:"How many seasons of 'Koffee with Karan' have been broadcasted till date?",
+            question:"What is Bunny's real name in the movie?",
             // question: "What was the name of Tia’s house helper in the movie?",
             model: "quest4",
 
             options: [{
                 "id": "radio1",
-                "name": "7"
+                "name": "Karan Sharma"
             }, {
                 "id": "radio2",
-                "name": "4"
+                "name": "Kabir Sharma"
             }, {
                 "id": "radio3",
-                "name": "8"
+                "name": "Kabir Thapar",
+                answer: true
             }, {
                 "id": "radio4",
-                "name": "5",
-                answer: true
+                "name": "Karan Thapar"
             }]
         }, {
             id: 5,
             quesNo: "Q5",
-            question:"How many films that Karan Johar has directed have titles starting with the letter 'K'?",
+            question:" Which of the following actors makes a cameo appearance in the movie?",
             // question: "Where did Arjun and Tia go for a date?",
             model: "quest5",
             options: [{
                 "id": "radio1",
-                "name": "5",
+                "name": "Sharman Joshi",
             }, {
                 "id": "radio2",
-                "name": "4"
+                "name": "Varun Dhawan"
             }, {
                 "id": "radio3",
-                "name": "3",
+                "name": "Rana Daggubati",
                  answer: true
             }, {
                 "id": "radio4",
-                "name": "5"
+                "name": "Sidharth Malhotra"
             }]
         }, {
             id: 6,
             quesNo: "Q6",
             // question: "Who was the person who witnessed the Kapoor showdown in their house while working?",
-            question:"The title of the short film directed by Karan Johar for 'Bombay Talkies' is?",
+            question:"Where does Avi spend his New Year's eve?",
             model: "quest6",
             options: [{
                 "id": "radio1",
-                "name": "Ajeeb Dastaan Hai Yeh",
-                answer: true 
+                "name": "In Udaipur"
             }, {
                 "id": "radio2",
-                "name": "Star"
+                "name": " In his bar",
+                answer: true 
             }, {
                 "id": "radio3",
-                "name": "Sheila Ki Jawaani"
+                "name": "In Manali"
             }, {
                 "id": "radio4",
-                "name": "Murabba"
+                "name": "In Paris"
             }]
         }, {
             id: 7,
             quesNo: "Q7",
-            question:"Which is Karan Johar's debut film as an actor?",
+            question:"To which place place do Avi, Aditi, Bunny and Naina travel for a brief holiday in the first half of the film?",
             // question: "What is Rajat Kapoor's name in the movie?",
             model: "quest7",
             options: [{
                 "id": "radio1",
-                "name": "Dil Toh Pagal Hai"
-            }, {
-                "id": "radio2",
-                "name": "Duplicate"
-            }, {
-                "id": "radio3",
-                "name": "Dilwale Dulhania Le Jayenge",
+                "name": "Manali",
                  answer: true
             }, {
+                "id": "radio2",
+                "name": "Ladakh"
+            }, {
+                "id": "radio3",
+                "name": "Switzerland"
+            }, {
                 "id": "radio4",
-                "name": "Bombay Velvet"
+                "name": "Alaska"
             }]
         }, {
             id: 8,
              quesNo: "Q8",
-             question:"On how many reality shows has Karan Johar appeared as a judge?",
+             question:"Which song in the movie is sung by Benny Dayal?",
             // question: " What was the wildest thing Rahul had done in his life?",
             model: "quest8",
             options: [{
                 "id": "radio1",
-                "name": " 3",
-                 answer: true
+                "name": "Ilahi"
             }, {
                 "id": "radio2",
-                "name": "1",
+                "name": "Dilli Wali Girlfriend",
             }, {
                 "id": "radio3",
-                "name": "2"
+                "name": "Ghagra"
             }, {
                 "id": "radio4",
-                "name": "4"
+                "name": "Badtameez Dil",
+                 answer: true
             }]
         }, {
             id: 9,
             quesNo: "Q9",
-               question:"Which was the latest film directed by Karan Johar?",
+               question:"The song Ilahi has been shot in which city?",
             // question: "Which song was played in the house party hosted by Tia?",
             model: "quest9",
             options: [{
                 "id": "radio1",
-                "name": "Ae Dil Hai Mushkil",
-                answer: true
+                "name": "Munich"
             }, {
                 "id": "radio2",
-                "name": "Student of the Year"
+                "name": "Paris",
+                answer: true
             }, {
                 "id": "radio3",
-                "name": "My Name Is Khan"
+                "name": "Barcelona"
             }, {
                 "id": "radio4",
-                "name": "Kapoor & Sons"
+                "name": "London"
             }]
         }, {
             id: 10,
             quesNo: "Q10",
-            question: "What is the title of Karan Johar's recently released autobiography?",
+            question: "Who is the director of the movie?",
             model: "quest10",
             options: [{
                 "id": "radio1",
-                "name": "Surprised by joy"
+                "name": "Karan Malhotra"
             }, {
                 "id": "radio2",
-                "name": "The Story of My Life"
+                "name": "Abhishek Varman"
             }, {
                 "id": "radio3",
-                "name": "An Unsuitable Boy",
+                "name": "Ayan Mukerji",
                 answer: true
             }, {
                 "id": "radio4",
-                "name": "Life as I see it"
+                "name": "Punit Malhotra"
             }]
         }];
         var answered;
