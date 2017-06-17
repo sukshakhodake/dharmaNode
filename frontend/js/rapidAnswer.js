fcc48228dc4eabe559feea6d3cca32626ba3aeed
sohan1 <shn619@gmail.com>
2017-06-17 11:18:00 +0530

    rapid.js

--- frontend/js/rapidAnswer.js
+++ frontend/js/rapidAnswer.js
@@ -7,207 +7,207 @@ var rapidAnswer = angular.module('rapidAnswer', [])
         var questions = [{
             id: 1,
             quesNo: "Q1",
-            question:"What is the name of Kalki Koechlin's character in the movie?",
+            question:"What is the profession of Vijay's father in Agneepath?",
             // question: "What was the surprise gift given by Rahul & Arjun to Dadu on his birthday?",
             model: "quest1",
             options: [{
                 "id": "radio1",
-                "name":"Naina"
+                "name":"Policeman"
                 // "name": "Cut out of Manadakini",
             }, {
                 "id": "radio2",
-                "name":"Aditi",
+                "name":"School teacher",
                   answer: true
                 // "name": "Cut out of Zeenat Aman"
 
             }, {
                 "id": "radio3",
                 // "name": "Cut out of Sridevi"
-                 "name":"Anjali"
+                 "name":"Doctor"
             }, {
                 "id": "radio4",
-                "name":"Neha"
+                "name":"Businessman"
                 // "name": "Cut out of Nargis"
             }]
         }, {
             id: 2,
             quesNo: "Q2",
-            question:"Which song do Bunny and Avi sing to wake up Aditi?",
+            question:"Which Dharma father says the following dialogue - 'Keh diya na, bas keh diya'?",
             // question: "What was the Kapoor family's pet dog's name?",
             model: "quest2",
             options: [{
                 "id": "radio1",
-                "name": " Badtameez Dil"
+                "name": "Harsh Kapoor"
             }, {
                 "id": "radio2",
-                "name": "Dilli wali Girlfriend"
+                "name": "Dinanath Chauhan"
             }, {
                 "id": "radio3",
-                "name": "Jumma Chumma De De",
+                "name": "Yashvardhan Raichand",
                  answer: true 
             }, {
                 "id": "radio4",
-                "name": "Saara Zamaana"
+                "name": "Bipin Arora"
             }]
         }, {
             id: 3,
             quesNo: "Q3",
-            question:"What is the name of the haunted hill that Bunny and Naina climb together?",
+            question:"What is the real name of 'Sexy Sam' in Kabhi Alvida Naa Kehna?",
             // question: "What was Rahul’s profession in the movie?",
             model: "quest3",
             options: [{
                 "id": "radio1",
-                "name": "Bhutha Parbat", 
-                answer: true
+                "name": "Sameer Singh"
             }, {
                 "id": "radio2",
-                "name": "Bhairav Parbat"
+                "name": "Samarjit Singh"
             }, {
                 "id": "radio3",
-                "name": "Kaalmani Pahad"
+                "name": "Sameer Talwar"
             }, {
                 "id": "radio4",
-                "name": "Raghav Pahad",
+                "name": "Samarjit Talwar", 
+                answer: true
             }]
         }, {
             id: 4,
             quesNo: "Q4",
-            question:"What is Bunny's real name in the movie?",
+            question:"Which of the following actors has never played the role of a father in a Dharma movie?",
             // question: "What was the name of Tia’s house helper in the movie?",
             model: "quest4",
 
             options: [{
                 "id": "radio1",
-                "name": "Karan Sharma"
+                "name": "Shah Rukh Khan"
             }, {
                 "id": "radio2",
-                "name": "Kabir Sharma"
+                "name": "Arjun Rampal"
             }, {
                 "id": "radio3",
-                "name": "Kabir Thapar",
+                "name": "Varun Dhawan",
                 answer: true
             }, {
                 "id": "radio4",
-                "name": "Karan Thapar"
+                "name": "Anupam Kher"
             }]
         }, {
             id: 5,
             quesNo: "Q5",
-            question:" Which of the following actors makes a cameo appearance in the movie?",
+            question:"In 'Shaandaar', Pankaj Kapur plays the father of?",
             // question: "Where did Arjun and Tia go for a date?",
             model: "quest5",
             options: [{
                 "id": "radio1",
-                "name": "Sharman Joshi",
-            }, {
-                "id": "radio2",
-                "name": "Varun Dhawan"
-            }, {
-                "id": "radio3",
-                "name": "Rana Daggubati",
+                "name": "Alia Bhatt",
                  answer: true
             }, {
+                "id": "radio2",
+                "name": "Shahid Kapoor"
+            }, {
+                "id": "radio3",
+                "name": "Shibani Dandekar"
+            }, {
                 "id": "radio4",
-                "name": "Sidharth Malhotra"
+                "name": "Sanjay Kapoor"
             }]
         }, {
             id: 6,
             quesNo: "Q6",
             // question: "Who was the person who witnessed the Kapoor showdown in their house while working?",
-            question:"Where does Avi spend his New Year's eve?",
+            question:"What does Ananya's father learn from Krish in 2 States?",
             model: "quest6",
             options: [{
                 "id": "radio1",
-                "name": "In Udaipur"
+                "name": "Driving a car"
             }, {
                 "id": "radio2",
-                "name": " In his bar",
+                "name": "Making a Powerpoint presentation",
                 answer: true 
             }, {
                 "id": "radio3",
-                "name": "In Manali"
+                "name": "Cooking food"
             }, {
                 "id": "radio4",
-                "name": "In Paris"
+                "name": "Playing tennis"
             }]
         }, {
             id: 7,
             quesNo: "Q7",
-            question:"To which place do Avi, Aditi, Bunny and Naina travel for a brief holiday in the first half of the film?",
+            question:"In which of the following films does Shah Rukh Khan not play the role of a father?",
             // question: "What is Rajat Kapoor's name in the movie?",
             model: "quest7",
             options: [{
                 "id": "radio1",
-                "name": "Manali",
-                 answer: true
+                "name": "Kuch Kuch Hota Hai"
             }, {
                 "id": "radio2",
-                "name": "Ladakh"
+                "name": "Kabhi Alvida Naa Kehna"
             }, {
                 "id": "radio3",
-                "name": "Switzerland"
+                "name": "Kabhi Khushi Kabhie Gham"
             }, {
                 "id": "radio4",
-                "name": "Alaska"
+                "name": "Kal Ho Naa Ho",
+                 answer: true
             }]
         }, {
             id: 8,
              quesNo: "Q8",
-             question:"Which song in the movie is sung by Benny Dayal?",
+             question:"In Humpty Sharma Ki Dulhania, what does Humpty dream of buying his father?",
             // question: " What was the wildest thing Rahul had done in his life?",
             model: "quest8",
             options: [{
                 "id": "radio1",
-                "name": "Ilahi"
+                "name": "A house"
             }, {
                 "id": "radio2",
-                "name": "Dilli Wali Girlfriend",
+                "name": "A boat",
             }, {
                 "id": "radio3",
-                "name": "Ghagra"
+                "name": "A car",
+                 answer: true
             }, {
                 "id": "radio4",
-                "name": "Badtameez Dil",
-                 answer: true
+                "name": "A bike"
             }]
         }, {
             id: 9,
             quesNo: "Q9",
-               question:"The song Ilahi has been shot in which city?",
+               question:"Who plays the role of Harsh Kapoor in Kapoor & Sons?",
             // question: "Which song was played in the house party hosted by Tia?",
             model: "quest9",
             options: [{
                 "id": "radio1",
-                "name": "Munich"
+                "name": "Rishi Kapoor"
             }, {
                 "id": "radio2",
-                "name": "Paris",
+                "name": "Rajat Kapoor",
                 answer: true
             }, {
                 "id": "radio3",
-                "name": "Barcelona"
+                "name": "Sidharth Malhotra"
             }, {
                 "id": "radio4",
-                "name": "London"
+                "name": "Fawad Khan"
             }]
         }, {
             id: 10,
             quesNo: "Q10",
-            question: "Who is the director of the movie?",
+            question: "How many Dharma movies has Amitabh Bachchan played the role of a father?",
             model: "quest10",
             options: [{
                 "id": "radio1",
-                "name": "Karan Malhotra"
-            }, {
-                "id": "radio2",
-                "name": "Abhishek Varman"
-            }, {
-                "id": "radio3",
-                "name": "Ayan Mukerji",
+                "name": "2",
                 answer: true
             }, {
+                "id": "radio2",
+                "name": "3"
+            }, {
+                "id": "radio3",
+                "name": "4"
+            }, {
                 "id": "radio4",
-                "name": "Punit Malhotra"
+                "name": "5"
             }]
         }];
         var answered;
