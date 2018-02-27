e6b7f1e54e46fff68c6cc58a8cf66a720298f2e1
Chintan <chintan@wohlig.com>
2018-02-27 12:37:27 +0530

    ok

--- api/responses/metaView.js
+++ api/responses/metaView.js
@@ -1,4 +1,4 @@
-module.exports = function(data, options) {
+module.exports = function (data, options) {
     var req = this.req;
     var res = this.res;
     var sails = req._sails;
@@ -6,20 +6,21 @@ module.exports = function(data, options) {
         data = {};
     }
     var env = require("../../config/env/" + sails.config.environment + ".js");
-        var obj = {
-            jsFiles: jsFiles,
-            title: data.title,
-            description: data.description,
-            keywords: data.keywords,
-            adminurl: env.realHost + "/api/",
-            image: env.realHost + "/api/download/" + data.image,
-            url: env.realHost + req.path,
-        };
-    
+    var obj = {
+        jsFiles: jsFiles,
+        title: data.title,
+        description: data.description,
+        keywords: data.keywords,
+        adminurl: env.realHost + "/api/",
+        image: env.realHost + "/api/download/" + data.image,
+        url: env.realHost + req.path,
+    };
+
 
     if (!data.image) {
         delete obj.image;
     }
-    // console.log(obj);
+    console.log(sails.config.environment);
+    console.log(obj);
     res.view(sails.config.environment, obj);
 };
